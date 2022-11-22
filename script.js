472b047cfb9bc22929f462330b26fba2e90dbb21
Patryk <wa51u6@gmail.com>
2022-11-22 06:26:13 +0100

    code cleanup done

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ let computerScore = 0
 let playerSelection; 
 let computerSelection; 
 
-game()
+game();
+
 
 function getUserChoice(){
     let userChoice ="";
     userChoice = prompt("Please choose your weapon: \n(rock scissors paper) ");
     userChoice = userChoice.toLowerCase();
     if  (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
-    return userChoice;
+        return userChoice;
     }else{
-    alert("wrong weapon, bye bye");
-    return userChoice = ("STOP");
+        alert("wrong weapon, bye bye");
+        return userChoice = ("STOP");
     };
 };
 
@@ -24,11 +25,11 @@ function getComputerChoice() {
     let random = Math.floor(Math.random() * 3)+1;
     console.log("number" + random);
     if (random == 1) {
-    return "rock";
+        return "rock";
     } else if (random == 2) {
-    return "paper";
+        return "paper";
     } else {
-    return "scissors";
+        return "scissors";
     };
 };
 
@@ -36,38 +37,37 @@ function getComputerChoice() {
  function playRound(playerSelection, computerSelection){
 
     if (playerSelection == computerSelection){
-    playerScore = playerScore + 1;
-    computerScore = computerScore + 1;
-    return ("Win-Win" + playerSelection);
+        playerScore = playerScore + 1;
+        computerScore = computerScore + 1;
+        return ("Win-Win" + playerSelection);
     } else if (
-    (playerSelection == "rock" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "rock") ){
-    playerScore = playerScore + 1;
-    alert ("player win battle!\n" + playerSelection + " destroy " + computerSelection);
-    }
-    else {
-    computerScore = computerScore + 1;
-    alert ("player loose battle!\n" + playerSelection + " is destroyed by: " + computerSelection);
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock") ){
+        playerScore = playerScore + 1;
+        alert ("player win battle!\n" + playerSelection + " destroy " + computerSelection);
+    } else {
+        computerScore = computerScore + 1;
+        alert ("player loose battle!\n" + playerSelection + " is destroyed by: " + computerSelection);
     };
-    };
+};
 
 function game(){
     for (let i = 0; i < 5; i++) {
-    playerSelection = getUserChoice();
-    if (playerSelection == "STOP"){
-    return;
-    };
-    computerSelection = getComputerChoice();
-    playRound(playerSelection,computerSelection);
-    alert("Round "+ (i + 1)  +" Score:\nPlayer Score: " + playerScore + " Computer score: " + computerScore);
+        playerSelection = getUserChoice();
+        if (playerSelection == "STOP"){
+            return;
+        };
+        computerSelection = getComputerChoice();
+        playRound(playerSelection,computerSelection);
+        alert("Round "+ (i + 1)  +" Score:\nPlayer Score: " + playerScore + " Computer score: " + computerScore);
     };
     if (playerScore > computerScore) {
-    let warResult = "WIN";
+        let warResult = "WIN";
     } else if (playerScore == computerScore) {
-    warResult = "DRAW";
+        warResult = "DRAW";
     } else {
-    warResult = "LOOSE";
+        warResult = "LOOSE";
     }
-    alert("player: " + warResult + " with computer\nPlayer Score: " + playerScore + " - Computer score: " + computerScore);
+        alert("player: " + warResult + " with computer\nPlayer Score: " + playerScore + " - Computer score: " + computerScore);
 };
