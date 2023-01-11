07bb7c4c82bb8a0ffba382e13e8eaa0bae342b35
Patryk <wa51u6@gmail.com>
2023-01-12 00:10:38 +0100

    first revision done

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-
-let playerScore = 0 
+let playerScore = 0
 let computerScore = 0
-
-let playerSelection; 
-let computerSelection; 
-let userChoice = "";
-
-
-
-
-/*
-function getUserChoice(){
-    let userChoice ="";
-    userChoice = prompt("Please choose your weapon: \n(rock scissors paper) ");
-    userChoice = userChoice.toLowerCase();
-    if  (userChoice == "rock" || userChoice == "paper" || userChoice == "scissors") {
-        return userChoice;
-    }else{
-        alert("wrong weapon, bye bye");
-        return userChoice = ("STOP");
-    };
-};
-*/
+let playerSelection
+let computerSelection
+let userChoice = ""
+let computerChoice = ""
+let result = ""
+let roundNr = 0
+let round = ""
+let warScore = ""
+let warResult = ""
 
 function getComputerChoice() {
-    let random = Math.floor(Math.random() * 3)+1;
-    console.log("number" + random);
+    let random = Math.floor(Math.random() * 3) + 1;
     if (random == 1) {
         return "rock";
     } else if (random == 2) {
@@ -35,82 +21,88 @@ function getComputerChoice() {
     };
 };
 
-
- function playRound(playerSelection, computerSelection){
-
-    if (playerSelection == computerSelection){
+function playRound(playerSelection, computerSelection) {
+    computerChoice = computerSelection
+    if (playerSelection == computerSelection) {
         playerScore = playerScore + 1;
         computerScore = computerScore + 1;
         return ("Win-Win" + playerSelection);
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock") ){
+        (playerSelection == "paper" && computerSelection == "rock")) {
         playerScore = playerScore + 1;
-        alert ("player win battle!\n" + playerSelection + " destroy " + computerSelection);
+        result = ("player win battle!\n" + playerSelection + " destroy " + computerSelection);
     } else {
         computerScore = computerScore + 1;
-        alert ("player loose battle!\n" + playerSelection + " is destroyed by: " + computerSelection);
+        result = ("player loose battle!\n" + playerSelection + " is destroyed by: " + computerSelection);
     };
 };
 
+function game(button) {
+    userChoice = (button);
+    playRound(button, getComputerChoice());
+    roundNr++;
+    round = ("Round " + (roundNr) + " Score:\nPlayer Score: " + playerScore + " Computer score: " + computerScore);
+    if (roundNr == 6){
+        divBoard.style.visibility='hidden';
 
-
-function game(){
-
-        playerSelection = getUserChoice();
-      
-        computerSelection = getComputerChoice();
-        playRound(playerSelection,computerSelection);
-       // alert("Round "+ (i + 1)  +" Score:\nPlayer Score: " + playerScore + " Computer score: " + computerScore);
-    };
-    if (playerScore > computerScore) {
-        let warResult = "WIN";
-    } else if (playerScore == computerScore) {
-        warResult = "DRAW";
-    } else {
-        warResult = "LOOSE";
+        window.location.reload();
+        divBoard.style.visibility='hidden';
     }
-      //  alert("player: " + warResult + " with computer\nPlayer Score: " + playerScore + " - Computer score: " + computerScore);
-
-
-/*
-rockBtn = document.querySelector('#rock');
-scissorsBtn = document.querySelector('#scissors');
-papperBtn = document.querySelector('#paper');
-
-
-
-
-
-rockBtn.addEventListener('click', rock)
-scissorsBtn.addEventListener('click', scissors)
-papperBtn.addEventListener('click', paper)
-
-function rock(){
-    console.log('rock')
+    dashBoardRefresh();       
+    if (roundNr == 5) {
+        if (playerScore > computerScore) {
+            warResult = "WIN";
+        } else if (playerScore == computerScore) {
+            warResult = "DRAW";
+        } else {
+            warResult = "LOOSE";
+        }
+        warScore = ("player: " + warResult + " with computer:\nPlayer Score: " + playerScore + " - Computer score: " + computerScore);
+        dashBoardRefresh();
+    };
 };
-function scissors(){
-    console.log('scissors')
-};
-function paper(){
-    console.log('paper')
-};
-*/
 
-buttons = document.querySelectorAll('button')
+buttons = document.querySelectorAll('button');
+divCont = document.querySelector('#buttonsContainter');
+divCont.style.display = "flex";
+divCont.style.justifyContent = "center";
+divCont.style.alignItems = "center";
+divCont.style.gap = "20px";
+divCont.style.margin = "20px";
 
 buttons.forEach(button => {
-button.addEventListener('click', () => {
-    UserChoice = (button.id)
-    console.log(button.id)
-    playRound(button.id, getComputerChoice())
-  });
+    button.addEventListener('click', () => {
+        game(button.id);
+    });
 });
 
+divBoard = document.querySelector('#scoreBoard');
+divBoard.style.display = "flex";
+divBoard.style.flexDirection = "column";
+divBoard.style.justifyContent = "center";
+divBoard.style.alignItems = "center";
 
+divSubBoard = document.createElement('div');
+divSubBoard.style.backgroundColor = "red";
+divSubBoard.style.margin = "20px";
+divBoard.appendChild(divSubBoard);
 
-function getUserChoice(){
-return userChoice
+divSubBoardTwo = document.createElement('div');
+divSubBoardTwo.style.margin = "10px";
+divBoard.appendChild(divSubBoardTwo);
+
+divSubBoardThree = document.createElement('div');
+divBoard.appendChild(divSubBoardThree);
+divSubBoardFour = document.createElement('div');
+divSubBoardFour.style.margin = "30px";
+divSubBoardFour.style.fontSize = "17px";
+divBoard.appendChild(divSubBoardFour);
+
+function dashBoardRefresh() {
+    divSubBoard.textContent = round;
+    divSubBoardTwo.textContent = `player weapon: ${userChoice},   computer weapon: ${computerChoice}`;
+    divSubBoardThree.textContent = result;
+    divSubBoardFour.textContent = warScore;
 }
-
